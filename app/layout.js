53c4ca562fb58c5eca373ab82fe3c69c36d3ebdf
ryoshi1001 <ryoshi16421@proton.me>
2025-02-07 02:58:24 -0800

    package.json updated

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from 'next/font/local';
 import { Roboto } from "next/font/google";
-
-
 
 const montserrat = localFont({
   src: './fonts/Montserrat-VariableFont_wght.ttf', 
@@ -19,15 +16,6 @@ const roboto = Roboto({
   display: 'swap'
 });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -38,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} ${roboto.variable} antialiased`}
+        className={`${montserrat.variable} ${roboto.variable} antialiased`}
       >
         {children}
       </body>
