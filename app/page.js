a888d6fe1dbde3177be5699e18ddfec282c7a32e
ryoshi1001 <ryoshi16421@proton.me>
2025-02-08 00:30:23 -0800

    weatherapp complete: backgraound image and padding top fixed for mobile

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
    <>
    <section 
-   className="w-full min-h-screen bg- sm:bg-cover bg-center bg-no-repeat flex items-center justify-center p-[1rem]"
+   className="w-full min-h-screen bg-cover bg-no-repeat flex items-center justify-center px-[1rem] py-[3rem]"
    style={{backgroundImage: "url('/weatherbg.jpg')"}}
    >
     <WeatherUI/>
